f827475749095977ea4dd2c69b22ed1547945c0a
imranh1987 <md.imranh1987@gmail.com>
2023-08-29 09:54:44 +0600

    run reactjs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    <!-- As you can see, we will use vite with jsx syntax for React-->
     @inertiaHead
 </head>
 <body>
-{{--<div id="app" data-page="{{ json_encode($page) }}"></div>--}}
-    @inertia
+@inertia
 </body>
 </html>
